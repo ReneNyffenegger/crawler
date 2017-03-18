02df66104311cfe5dbd4904c609e687f965cdf3f
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-03-18 07:59:29 +0100

    2017-03-17

--- Preise/Migros/go.pl
+++ Preise/Migros/go.pl
@@ -1,4 +1,28 @@
 #!/usr/bin/perl
+use utf8;
+use Encode;
+binmode(STDOUT, ":utf8");
+
+my $print_ids = 1;
+
+#
+#   https://catalog.leshop.ch/catalog/public/v1/api/compatibility/categories/35961,29154?language=de
+#     {"29154":{"id":29154,"parentId":25131,"name":{"de":"Milch"},"slug":{"de":"milch"},"sortPriority":1,"path":"/Le Shop/Map_Dairy/Dairy/Milk","categories":[29160,29156,29159,34206,29580,35961],"displayPromotionsOnly":false},
+#      "35961":{"id":35961,
+#               "parentId":29154,
+#               "name":{"de":"Kondensmilch"},
+#               "slug":{"de":"kondensmilch"},
+#               "sortPriority":6,
+#               "path":"/Le Shop/Map_Dairy/Dairy/Milk/Condensedmilk"
+#               ,"productsUndeliverabilities":{
+#                  "51572":{},
+#                  "217413":{"warehouses":[1]}},
+#                  "displayPromotionsOnly":false}
+#      }
+#
+#   https://catalog.leshop.ch/catalog/public/v1/api/compatibility/products/51572,217413?language=de&shortVersion=true
+#   https://catalog.leshop.ch/catalog/public/v1/api/compatibility/prices/51572,217413/warehouses/2
+#
 use 5.10.0;
 use warnings;
 use strict;
@@ -21,66 +45,211 @@ my $mech = WWW::Mechanize->new(
 
 
 my $leshopch_token = 
-    'eyJsdmwiOiJVIiwiZW5jIjoiQTI1NkdDTSIsImFsZyI6ImRpciIsImtpZCI6IjQyIn0..Gj2s2ZMCOpisIOwP.9mkg11M-zCMBZ3X2AAOmMII3GIZyog9xtsQ5C36z3niIwgdwEuyC0WSE_mnfyF2MdsoWZyKaQhZ77Y7rGc0WogkGpwJeVvTX40d7bqEn9dUjKNLblansGaCk88Iuk8nLXzHwDfdkOjZm6wD2ZKwj5n6Uby-AZrFk7NiovHwX2KPNZbQkQZpbiPtUUMgJkj0gq6o-6tI_OtsCOIzdL9tpbpSqIIp66L28yfdVUQ398NoxhpM_dgo_nw8gz-aIiBSPTTvk50_uFgR84n7uPu7r3rhVBXYzZCv4RM5PfQzwodPzvKiZbEP0GuVY7IwI2e-Qv3OCMg3WQ2AIrDIWCzPdrR5xza8pZdS1Gl_JwuAmxl-TZ8AUCcAcCSIgZTaNWfdZIpZAeV_pWXBLukXfYjEgu08HABIwR6D2tBUDF0MhVLPw9A.GJvQeTPLqS2eT8fMK1Yn_A'
-  # 'eyJsdmwiOiJVIiwiZW5jIjoiQTI1NkdDTSIsImFsZyI6ImRpciIsImtpZCI6ImRlZmF1bHQifQ..yis5B69SsNjFoRWJ.ETCnibanQoumCAKl2dawwhL59qo7cQF0r2G42ld5GJm6IpiSH5ni1Rx2sxZI5tFW0s5U6ItQ2yognyHWOF9Y143fgQegE1UFj1nCi4yOS2e2IZN1-UxIT4BoOmhxbrj7y85__2rxnqeXMkaDsk5Dkdm0LmvbthIV7F6h6Jhdz_OqJvYNzGRS2BQtj_1sSgzPC0c6z2tdTXRwOXxqIsL0vyt1ukEv0NNKAI7HcewgaLYPTa8irLuQrcbwtg62jSNmKfm2ZAGbAe94twwQHczM5lE1SInMtcT4giz_lRPUq7A-Rwz9NoDvs5kH6_ZW3lN6u6aLpeGLJ62W0qXwhut7z8Oquu6GNJAJx9XcsAg6jK2-Qsj5BG58D-5notYHKOwVbIBJSzTdgciOHq3Nt5pNdaLmoKmNYyPCfEGwu8PeRfvKGA.73ThZaH8OXztWKRQCkY5JA';
-  # 'eyJsdmwiOiJVIiwiZW5jIjoiQTI1NkdDTSIsImFsZyI6ImRpciIsImtpZCI6IjQyIn0..8giY8Tk_tXxU4H4J.97u0ikyCWyJVLTh34ReYCx2tLaVAx5Kb1t-z8BrXq4eO000HkYry6tGtdD3VQ1bwaE07a6quXbm3z0emvWNyUuxPvrFPL-xHdYs6ZEcntcizMeuL-9qTC0huTvAYNad3Xh2Cms7gcj-_iBF1eaZegx3F2hH7dzfpBNe-EnhOLLGVPqrN_-HzTYOuMDacSDErkC6jv5tjVAxOplwihjDfBv1srrC6RsHq9pda36XBTdzUMAqUGTZaBrBjPzhhrVYpwzlQo5KuH48ZvWVTzx9QCeQgVD-PYFQVU9LtE7TNev8dp78ruEtu5-bnkOipKmuit5qpTSWAFHbHOOe5mveJRg_Y_xE-eEqRD7lIPKqXcrMFhuUpHzsMnq-D86YOCZcEEN1LwXB5JfdTd_aiDNT5N1a266nnOKkaS7k5fFrzc3U2eQ.uZgd62jrEr0YJTxZT2Uyyg');
+'eyJsdmwiOiJVIiwiZW5jIjoiQTI1NkdDTSIsImFsZyI6ImRpciIsImtpZCI6ImRlZmF1bHQifQ..TJ67skeyWV2_160V.6zXt1TuSvHMBSRaCQQrkQ3OBGBRcge4IyY5_aM6AZEwTZqMX58REFvDnb1Qj2O1o_d22k8iW5TdT4L0PuuZORk0WXk_a3gjAqxtHrw3q5Lx54GNksd1Lq6kZq-_hRFHl_4rTdMiy80W0mUTcky_98bogud54sum_v-BHHByh4IHxdaT1r4P3_abSCeOlBr_xV3bMiJDIlDVmaVgvUTnDAub5bu5Ty7jASbwyCT48LdZQBEZemFSpZt06nFNdOIjPJn4RJzwFMJeieYsqetnztD41uLN1UX7ciTfPb0xA9AR4DmY8aUd5oGpvu64EaBzRaGc-gzYY9cxz2FzQLzAiwT8fBcfPyk4JRx6fei9atGmv0cr2FGzftn3_LX-DltBLJ0IAcrApQ_MjJy6nrNGeDxSdy4NQ-oyqqJ7CXeuWpsb3yw.87qDps1D2KwvvEvKjZlHzw'
 ;
 
 my $cookie = 
   'leshop-tgt=j4OEzOQMamBefH5MHqR097YBvlVhLFLoS8b8bxvHXE4giTXZPoCkwDYCIN_jPh5rm9MiIi1WO-WPkyhNxvHG27PsmOOHtL8gMvVOLBORdfMNE3stKg8-mWJjXpmfGND0|MTQ4OTU2OTg1Mg|U0gxQVMxMjhDQkM|DlU_osREzit212MzUYPTug|ismPsICHMuMEjmEPIfe2b-QtyGA; _ga=GA1.2.1505309499.1489569859; leshop-routing=a; GEAR=58208fa837425a95150000a1-prod; _gat=1'
-#                                                                                                                                                                                                                                '_ga=GA1.2.1505309499.1489569859; leshop-routing=a; GEAR=local-58208e2dcf67b62bc20000d7';
-# 'leshop-tgt=j4OEzOQMamBefH5MHqR097YBvlVhLFLoS8b8bxvHXE4giTXZPoCkwDYCIN_jPh5rm9MiIi1WO-WPkyhNxvHG27PsmOOHtL8gMvVOLBORdfMNE3stKg8-mWJjXpmfGND0|MTQ4OTU2OTg1Mg|U0gxQVMxMjhDQkM|DlU_osREzit212MzUYPTug|ismPsICHMuMEjmEPIfe2b-QtyGA; _ga=GA1.2.1505309499.1489569859; leshop-routing=a; GEAR=58208fa837425a95150000a1-prod; _gat=1'
 ;
 
-# my $request  = HTTP::Request->new('GET' => 'https://authentication.leshop.ch/authentication/public/v1/api/validate');
-# $request -> header('leshopch', 'eyJsdmwiOiJVIiwiZW5jIjoiQTI1NkdDTSIsImFsZyI6ImRpciIsImtpZCI6IjQyIn0..8giY8Tk_tXxU4H4J.97u0ikyCWyJVLTh34ReYCx2tLaVAx5Kb1t-z8BrXq4eO000HkYry6tGtdD3VQ1bwaE07a6quXbm3z0emvWNyUuxPvrFPL-xHdYs6ZEcntcizMeuL-9qTC0huTvAYNad3Xh2Cms7gcj-_iBF1eaZegx3F2hH7dzfpBNe-EnhOLLGVPqrN_-HzTYOuMDacSDErkC6jv5tjVAxOplwihjDfBv1srrC6RsHq9pda36XBTdzUMAqUGTZaBrBjPzhhrVYpwzlQo5KuH48ZvWVTzx9QCeQgVD-PYFQVU9LtE7TNev8dp78ruEtu5-bnkOipKmuit5qpTSWAFHbHOOe5mveJRg_Y_xE-eEqRD7lIPKqXcrMFhuUpHzsMnq-D86YOCZcEEN1LwXB5JfdTd_aiDNT5N1a266nnOKkaS7k5fFrzc3U2eQ.uZgd62jrEr0YJTxZT2Uyyg');
-# $request -> header('Cookie'  , 'leshop-tgt=j4OEzOQMamBefH5MHqR097YBvlVhLFLoS8b8bxvHXE4giTXZPoCkwDYCIN_jPh5rm9MiIi1WO-WPkyhNxvHG27PsmOOHtL8gMvVOLBORdfMNE3stKg8-mWJjXpmfGND0|MTQ4OTU2OTg1Mg|U0gxQVMxMjhDQkM|DlU_osREzit212MzUYPTug|ismPsICHMuMEjmEPIfe2b-QtyGA; _ga=GA1.2.1505309499.1489569859; leshop-routing=a; GEAR=58208fa837425a95150000a1-prod; _gat=1');
-# do_request($request);
 
 
 
-goto XXXXX;
+do_request('GET', 'https://catalog.leshop.ch/catalog/public/v1/api/compatibility/lightCategories?language=de', 'catalog.json');
 
-# my $request = HTTP::Request->new('GET' => 'https://catalog.leshop.ch/catalog/public/v1/api/compatibility/products/125531,215771,230738,62966,215888,205839,59029,49960,9902,229247,98077,215889,94678,70794,3501895,130507,112821,221697,3378442,142094,126632,3313519,225714,225087,104596,3534691,16380,46526,65147,125901,3505600,78984,98026,98749,3321740,3004264,227552,3155517,59479?language=de&shortVersion=true');
-# my $request = HTTP::Request->new('GET' => 'https://catalog.leshop.ch/catalog/public/v1/api/compatibility/prices/125531,215771,230738,62966,215888,205839,59029,49960,9902,229247,98077,215889,94678,70794,3501895,130507,112821,221697,3378442,142094,126632,3313519,225714,225087,104596,3534691,16380,46526,65147,125901,3505600,78984,98026,98749,3321740,3004264,227552,3155517,59479/warehouses/2');
-  my $request = HTTP::Request->new('GET' => 'https://catalog.leshop.ch/catalog/public/v1/api/compatibility/lightCategories?language=de');
-$request -> header('leshopch', $leshopch_token);
-$request -> header('Cookie'  , );
-my $content_catalog = do_request($request);
+# do_request('GET', 'https://catalog.leshop.ch/catalog/public/v1/api/compatibility/categories/35961?language=de', 'cat-35961');
 
-open (my $out, '>:encoding(utf-8)', "$ENV{digitales_backup}crawler/Preise/Migros/wgetted/catalog.json") or die;
-print $out $content_catalog;
-close $out;
 
-XXXXX:
 
-my $json = read_json_file("$ENV{digitales_backup}crawler/Preise/Migros/wgetted/catalog.json") or die;
+# XXXXX:
+
+my $json = read_wgetted_json_file('catalog.json') or die;
 
 my $tree = build_catalog_tree($json);
 
-$tree->traverse(sub {
+#_{ Get »le Shop« tree
+my $le_shop_tree;
+for my $cat ($tree->getAllChildren()) {
+# print $cat->getNodeValue()->{name}, "\n";
+  if ($cat->getNodeValue()->{name} eq 'Le Shop') {
+#   print "yep\n";
+    $le_shop_tree = $cat->clone();
+    last;
+  }
+}
+ #_}
+
+#_{ Remove »undeliverable« categery
+my @undeliverables_to_remove;
+for my $cat ($le_shop_tree->getAllChildren()) {
+  my $v = $cat->getNodeValue();
+  if ($v->{undevilerability}) {
+    push @undeliverables_to_remove, $cat;
+#   print "Remove $v->{name}\n";
+  }
+  else {
+#   print "Keep $v->{name}\n";
+  }
+}
+
+$le_shop_tree->removeChild($_) for (@undeliverables_to_remove);
+#_}
+
+$le_shop_tree->traverse(sub { #_{ Download price and catalog items
+  my $node = shift;
+
+  if ($node->isLeaf()) { #_{
+    my $v = $node->getNodeValue();
+    my $cat=$v->{id};
+    do_request('GET', "https://catalog.leshop.ch/catalog/public/v1/api/compatibility/categories/$cat?language=de", "cat-$cat");
+
+    my $json_cat = read_wgetted_json_file("cat-$cat");
+
+    for my $prod_id (keys %{$json_cat->{$cat}->{productsUndeliverabilities}}) {
+
+      do_request('GET', "https://catalog.leshop.ch/catalog/public/v1/api/compatibility/products/$prod_id?language=de&shortVersion=true", "prod-$prod_id");
+      do_request('GET', "https://catalog.leshop.ch/catalog/public/v1/api/compatibility/prices/$prod_id/warehouses/2", "prod-$prod_id-price");
+
+      my $json_prod        = read_wgetted_json_file("prod-$prod_id"      )->{$prod_id};
+      my $json_prod_price  = read_wgetted_json_file("prod-$prod_id-price")->{$prod_id};
+
+      $node -> generateChild(
+          {name     =>$json_prod->{name}{de},
+           nof_units=>$json_prod->{numberOfUnits},
+           id       =>$prod_id,
+           prod     =>1,
+           price    => {min      => $json_prod_price->{price}{minimum},
+                        max      => $json_prod_price->{price}{maximum},
+                        est      => $json_prod_price->{price}{estimated},
+                        exact    => $json_prod_price->{price}{exact},
+                        unit     => $json_prod_price->{unitPrice}{unit},
+                        per_unit => $json_prod_price->{unitPrice}{price} },
+           active   => $json_prod_price->{active}
+
+         });
+
+    }
+
+
+  } #_}
+
+}); #_}
+
+#_{ Print to HTML
+open (my $out, '>:encoding(utf-8)', '../Migros.html') or die; #_{
+  print $out "<html><head>
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+    <meta name='description' content='Preisliste Migros'>
+    <title>Preisliste Migros</title>
+
+    <style type='text/css'> 
+    .r0 {background-color:#ffffe7;}
+    .r1 {background-color:#ffe7ff;}
+    .st {color: #777;}
+    </style>
+
+  </head><body>
+  <h1>Preisliste Coop</h1>";
+  print $out "<pre><code>"
+  ; #_}
+
+my $r = 0;
+$le_shop_tree->traverse(sub { #_{
+
   my $node = shift;
   my $v = $node->getNodeValue();
-  print '  ' x $node->getDepth() . $v -> {name} . ' ' . $v->{id} . ' ' . $v->{parentId} . ' ' . $v->{undevilerability}. "\n";
 
-});
+  my $id = '';
+  if ($print_ids) {
+    $id = "ID: $v->{id}";
+  }
+
+  if ($v->{prod}) {
+
+     $r = 1-$r;
+     print $out "<span class='r$r'>";
 
 
-# my $request  = HTTP::Request->new('POST' => 'https://authentication.leshop.ch/authentication/public/v1/api/tickets/token');
-# $request = HTTP::Request->new('GET' => 'https://authentication.leshop.ch/authentication/public/v1/api/guest?rememberme=true');
-# $request -> header('origin'   , 'https://www.leshop.ch/');
-# $request -> header('referer'  , 'https://www.leshop.ch/');
-# $request -> header('Mime-Type', 'application/json');
+     print  $out " " x 10;
 
+     my $price;
+     if ($v->{price}{exact}) {
+       $price = sprintf "    %7.2f", $v->{price}{est}
+     }
+     else {
+       $price = sprintf("ca. %7.2f (%0.2f-%0.2f)", $v->{price}{est}, $v->{price}{min}, $v->{price}{max});
+     }
+
+     $v->{price}{unit} = 'St.' if $v->{price}{unit} eq 'piece';
+
+#    printf $out "%-120s | %2d Stück: %7.2f %7.2f %7.2f (%1s) | %7.2f/%-5s | %-5s $id",
+     printf $out "%-120s | <span class='st'>%2i Stück:</span> %-26s | %7.2f/%-5s $id",
+             $v->{name}, $v->{nof_units}, $price, $v->{price}{per_unit}, $v->{price}{unit}
+#                          $v->{nof_units}, 
+#                                $v->{price}{min},
+#                                      $v->{price}{max},
+#                                            $v->{price}{est},
+#                                                   $v->{price}{exact},
+#                                                           $v->{price}{per_unit},
+#                                                                 $v->{price}{unit},
+#                                                                        $v->{active}
+                                                                        ;
+      
+
+#    if ($v->{price}{min} != $v->{price}{max}  or
+#        $v->{price}{min} != $v->{price}{est}) {
+
+#        print $out " * oh oh *";
+#    
+#    }
+     print $out "</span>";
+
+  }
+  else {
+# print '  ' x $node->getDepth() . $v -> {name} . ' ' . $v->{id} . ' ' . $v->{parentId} . ' ' . $v->{undevilerability}. "\n";
+# print '  ' x $node->getDepth() . $v -> {name} . ' ' . $v->{id} . "\n";
+
+
+    print $out '  ' x $node->getDepth() . $v -> {name} . "  $id"; #              . "\n";
+  }
+  print $out "\n";
+
+}); #_}
+
+print $out "</pre></code>\n";
+print $out "<p>
+Vgl <a href='Coop.html'>Preisliste Coop</a>
+
+</html>";
+
+#_}
 
 sub do_request { #_{
 
-  my $request  = shift;
+  my $method       = shift;
+  my $url          = shift;
+# my $request      = shift;
+  my $wgetted_file = shift;
+
+  my $archive_file = "$ENV{digitales_backup}crawler/Preise/Migros/wgetted/$wgetted_file";
+  return if -f $archive_file;
+
+  print "$method $url -> $archive_file\n";
+
+  my $request = HTTP::Request->new($method, $url);
+  $request -> header('leshopch', $leshopch_token);
+  $request -> header('Cookie'  , );
+
   my $response = $mech->request($request);
-  show_http_headers_request_and_response($response);
+# show_http_headers_request_and_response($response);
+
+  open (my $out, '>:encoding(utf-8)', $archive_file) or die;
+  print $out decode("utf-8", $response->decoded_content);
+  close $out;
+
 
 # print $response->content;
-  return $response->decoded_content;
+# return $response->decoded_content;
 
 
 } #_}
@@ -112,17 +281,35 @@ sub show_http_headers_request_and_response { #_{
 
 } #_}
 
-sub read_json_file { #_{
+sub read_wgetted_json_file { #_{
   my $filename = shift;
-  open (my $in, '<:encoding(utf-8)', $filename) or die;
+  my $archived_file = "$ENV{digitales_backup}crawler/Preise/Migros/wgetted/$filename";
+  open (my $in, '<:encoding(utf-8)', $archived_file) or die;
   my $json_text = join '', <$in>;
   close $in;
 
-  return from_json($json_text);
+  my $json;
+  eval {
+    $json = from_json($json_text);
+  };
+  if ($@) {
+     unlink $archived_file;
+     die "$archived_file seemed not to be a JSON file";
+  }
+
+  if (exists $json->{'leshop-error'}) {
+
+    system "cat $archived_file";
+
+    unlink $archived_file;
+    die "leshop-error for $archived_file\n";
+  }
+
+  return $json;
   
 } #_}
 
-sub build_catalog_tree {
+sub build_catalog_tree { #_{
 
   my $json_catalog = shift;
 
@@ -148,9 +335,7 @@ sub build_catalog_tree {
 
   }; #_}
 
-  for my $id (keys %$json) {
-  
-#   if ($id > 0) {
+  for my $id (keys %$json) { #_{
   
       my $parentId  = $json->{$id}{parentId} // '';
       my $name      = $json->{$id}{name}{de};
@@ -159,9 +344,7 @@ sub build_catalog_tree {
 
       $undevilerability = 1 if exists $json->{$id}{undeliverability};
 
-#     $name .= " ($id -> $parentId )";
 
-#     my $parent_tree = &$get_or_make_tree($parentId);
       my $cur_tree    = &$get_or_make_tree($id);
       $cur_tree->setNodeValue({name => $name, id=>$id, parentId=>$parentId, undevilerability=>$undevilerability});
 
@@ -170,31 +353,9 @@ sub build_catalog_tree {
 
         $cur_tree->addChild($child_tree);
 
-#     }
-  
-#     $parent_tree->addChild($cur_tree);
-  
-  #   my $ids_seen{$k} = Tree::Simple->new($json->{$k}{name}{de};
-  
-  
-  #   if (my $parent_tree = $ids_seen{}) {
-  #     $parent_tree->addChild($ids_seen{$k});
-  #   }
-  #   else {
-  #     $ids_seen{$json->{$k}{parentId}} = Tree::Simple->new{
-  #     Tree::Simple->new($json->{$k}{name}{de});
-  #   }
-  
-  #   die unless $ids_seen{$json->{$k}{parentId}};
-  #   print ": Parent : ", $json->{$k}{parentId};
-    }
-#   else {
-#     $ids_seen{0} = $tree;
-#   }
-  # print "\n";
-  }
+      }
 
-
+  } #_}
   return $tree;
 
-}
+} #_}
